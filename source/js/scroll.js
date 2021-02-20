ca00e11be51294536705c73a1932bb5ebf43c684
timeliar <blood1679@outlook.com>
2021-02-20 16:41:56 +0800

    comment theme

---
+++ source/js/scroll.js
@@ -0,0 +1,28 @@
+$(function () {
+  const navBarHeight = $(".navbar").height();
+  if (!navBarHeight || !$(".navbar").hasClass("is-fixed-top")) {
+    navBarHeight = 0;
+  }
+  let hash = window.location.hash.substr(1);
+  if (hash) {
+    let hashItem = $("#" + decodeURIComponent(hash));
+    if (hashItem.length) {
+      let topOffset = hashItem.offset().top;
+      scrollAnimate(topOffset - navBarHeight);
+    }
+  }
+  $(".menu-list-link").click(function (e) {
+    e.preventDefault();
+    let topOffset = $(decodeURIComponent($.attr(this, "href"))).offset().top;
+    scrollAnimate(topOffset - navBarHeight);
+  });
+});
+
+function scrollAnimate(top) {
+  $("html, body").animate(
+    {
+      scrollTop: top - 10,
+    },
+    300
+  );
+}
